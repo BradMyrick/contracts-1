e9efb6ad169fd8f67d62873951c868e87e73244f
Brad Myrick <55957519+BradMyrick@users.noreply.github.com>
2022-07-15 09:26:45 -0500

    rxg division on creation to 3 wallets, test updated

--- contracts/rxgSmall.sol
+++ contracts/rxgSmall.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract Recharge is ERC20Burnable {
-// variables
-    uint256 public maxSupply = 10000000000 ether; // max supply of rxg
-    uint256 public unmintedSupply; // total supply of rxg pegged token that can be sold
 // constructor 
-    constructor(
-    ) ERC20("Recharge", "RXG") {
-        uint256 initialSupply = 10000000000 ether;
-        _mint(msg.sender, initialSupply);
+    constructor(address _one, address _two, address _three) ERC20("Recharge", "RXG") {
+        uint256 ammountOne = 1000000000 ether;
+        uint256 ammountTwo = 1000000000 ether;
+        uint256 ammountThree = 8000000000 ether;
+        _mint(_one, ammountOne);
+        _mint(_two, ammountTwo);
+        _mint(_three, ammountThree);
     }
 }
--- test/rxgSmallTest.js
+++ test/rxgSmallTest.js
@@ -5,18 +5,19 @@ const { parseEther } = require("ethers/lib/utils");
 describe ("RxgVending contract", function () {
     let RxgToken;
     let rxgToken;
+    let rxgSupply = parseEther("10000000000");
 
     beforeEach(async function () {
-        [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+        [owner, addr1, addr2, addr3, ...addrs] = await ethers.getSigners();
         RxgToken = await ethers.getContractFactory("Recharge");
-        rxgToken = await RxgToken.connect(owner).deploy();
+        rxgToken = await RxgToken.connect(owner).deploy(addr1.address, addr2.address, addr3.address);
         await rxgToken.deployed();
     }
     );
 
     describe("Deployment", function () {
         it("Should mint correct tokens to the creator", async function () {
-            expect(await rxgToken.balanceOf(owner.address)).to.equal(parseEther("10000000000"));
+            expect(await rxgToken.totalSupply()).to.equal(rxgSupply);
         }
         );
     }
