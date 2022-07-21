5d3d8be24f46a49032826b64e66bf4d38e405990
Brad Myrick <55957519+BradMyrick@users.noreply.github.com>
2022-07-21 11:31:01 -0500

    royalty tests

--- test/Tac721aTest.js
+++ test/Tac721aTest.js
@@ -1,6 +1,13 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
-const { parseEther } = require("ethers/lib/utils");
+const {
+    expect
+} = require("chai");
+const {
+    ethers
+} = require("hardhat");
+const {
+    parseEther
+} = require("ethers/lib/utils");
+const { BigNumber } = require("ethers");
 
 describe("Tacvue721a contract", function () {
     let Tacvue721a;
@@ -10,6 +17,7 @@ describe("Tacvue721a contract", function () {
     let name = "Tacvue721a";
     let ticker = "TACV";
     let maxMints = 5;
+    let royalty = 200; // should be 2% of sale price
     let maxSupply = 100;
     let mintPrice = parseEther("1");
     let wlPrice = parseEther("0.5");
@@ -18,108 +26,135 @@ describe("Tacvue721a contract", function () {
     beforeEach(async function () {
         Tacvue721a = await ethers.getContractFactory("Tacvue721a");
         [owner, feeCollector, addr1, addr2, ...addrs] = await ethers.getSigners();
-        tacvue721a = await Tacvue721a.connect(owner).deploy( name, ticker, maxMints, maxSupply, mintPrice, wlPrice, placeholderUri, feeCollector.address);
+        tacvue721a = await Tacvue721a.connect(owner).deploy(name, ticker, royalty, maxMints, maxSupply, mintPrice, wlPrice, placeholderUri, feeCollector.address);
         await tacvue721a.deployed();
-    }
-    );
+    });
     describe("Deployment", function () {
         it("Should set the right owner", async function () {
             expect(await tacvue721a.owner()).to.equal(owner.address);
         });
-    } 
-    );
+    });
 
     describe("Mint", function () {
         it("Should mint a new token if public sale IS active and Price is paid", async function () {
             await tacvue721a.connect(owner).saleActiveSwitch();
             expect(await tacvue721a.saleActive()).to.equal(true);
-            await tacvue721a.connect(addr1).mint(1, {value: ethers.utils.parseEther("1")});
-                expect(await tacvue721a.balanceOf(addr1.address)).to.equal(1);
-        } 
-        );
+            await tacvue721a.connect(addr1).mint(1, {
+                value: ethers.utils.parseEther("1")
+            });
+            expect(await tacvue721a.balanceOf(addr1.address)).to.equal(1);
+        });
         it("Should revert if sale is NOT active", async function () {
             expect(await tacvue721a.saleActive()).to.equal(false);
-            await(expect(tacvue721a.connect(addr1).mint(1, {value: ethers.utils.parseEther("1")}))).to.be.reverted;
-            }
-        );
+            await (expect(tacvue721a.connect(addr1).mint(1, {
+                value: ethers.utils.parseEther("1")
+            }))).to.be.reverted;
+        });
         it("Should revert if the minter is NOT WhiteListed and WL is active", async function () {
             await tacvue721a.connect(owner).wlActiveSwitch();
             expect(await tacvue721a.wlActive()).to.equal(true);
             expect(await tacvue721a.WhiteList(addr1.address)).to.equal(false);
-            await expect(tacvue721a.connect(addr1).mint(1, {value: ethers.utils.parseEther("1")})).to.be.reverted;
-            }
-        );
+            await expect(tacvue721a.connect(addr1).mint(1, {
+                value: ethers.utils.parseEther("1")
+            })).to.be.reverted;
+        });
         it("Should mint if the minter IS WhiteListed and WL is active.", async function () {
             await tacvue721a.connect(owner).bulkWhitelistAdd([addr1.address]);
             await tacvue721a.connect(owner).wlActiveSwitch();
             expect(await tacvue721a.connect(addr1).wlActive()).to.equal(true);
-            await tacvue721a.connect(addr1).mint(1, {value: ethers.utils.parseEther("1")});
+            await tacvue721a.connect(addr1).mint(1, {
+                value: ethers.utils.parseEther("1")
+            });
             expect(await tacvue721a.balanceOf(addr1.address)).to.equal(1);
-            }        
-        );
+        });
         it("Should revert if the minter is NOT WhiteListed and WL IS active", async function () {
             await tacvue721a.connect(owner).wlActiveSwitch();
             expect(await tacvue721a.wlActive()).to.equal(true);
             expect(await tacvue721a.WhiteList(addr1.address)).to.equal(false);
-            await expect(tacvue721a.connect(addr1).mint(1, {value: ethers.utils.parseEther("1")})).to.be.reverted;
-            }
-        );
-    }
-    );
+            await expect(tacvue721a.connect(addr1).mint(1, {
+                value: ethers.utils.parseEther("1")
+            })).to.be.reverted;
+        });
+    });
     describe("Whitelist", function () {
         it("Should add a new address to the WhiteList", async function () {
             await tacvue721a.connect(owner).bulkWhitelistAdd([addr1.address]);
             expect(await tacvue721a.WhiteList(addr1.address)).to.equal(true);
-            }
-        );
+        });
         it("Should leave the user whitelisted if the address is already in the WhiteList", async function () {
             await tacvue721a.connect(owner).bulkWhitelistAdd([addr1.address]);
             await tacvue721a.connect(owner).bulkWhitelistAdd([addr1.address]);
             expect(await tacvue721a.WhiteList(addr1.address)).to.equal(true);
-            }
-        );
+        });
         it("Should return false if the address is not in the WhiteList", async function () {
             expect(await tacvue721a.WhiteList(addr1.address)).to.equal(false);
-            }
-        );
+        });
         it("Should remove an address from the WhiteList", async function () {
             await tacvue721a.connect(owner).bulkWhitelistAdd([addr1.address]);
             expect(await tacvue721a.WhiteList(addr1.address)).to.equal(true);
             await tacvue721a.connect(owner).removeFromWhiteList(addr1.address);
             expect(await tacvue721a.WhiteList(addr1.address)).to.equal(false);
-            }
-        );
+        });
         it("Should revert if the address is not in the WhiteList", async function () {
             await expect(tacvue721a.connect(owner).removeFromWhiteList(addr1.address)).to.be.reverted;
-            }
-        );
-    }
-    );
+        });
+    });
 
     describe("Withdraw", function () {
         it("Should withdraw the correct amount of tokens", async function () {
             await tacvue721a.connect(owner).saleActiveSwitch();
-            await tacvue721a.connect(addr1).mint(1, {value: ethers.utils.parseEther("1")});
+            await tacvue721a.connect(addr1).mint(1, {
+                value: ethers.utils.parseEther("1")
+            });
             expect(await ethers.provider.getBalance(tacvue721a.address)).to.equal(ethers.utils.parseEther("1"));
             const tx = await tacvue721a.connect(owner).withdraw();
             const receipt = await tx.wait()
             for (const event of receipt.events) {
                 console.log(`Event ${event.event} with args ${event.args}`);
-              }
-        }
-        );
+            }
+        });
         it("Should revert if the address is not owner", async function () {
             await tacvue721a.connect(owner).saleActiveSwitch();
-            await tacvue721a.connect(addr1).mint(1, {value: ethers.utils.parseEther("1")});
+            await tacvue721a.connect(addr1).mint(1, {
+                value: ethers.utils.parseEther("1")
+            });
             await expect(tacvue721a.connect(addr1).withdraw()).to.be.reverted;
-            }
-        );
+        });
         it("Should revert if the address has no tokens", async function () {
             await expect(tacvue721a.connect(owner).withdraw()).to.be.reverted;
-            }
-        );
-    }
-    );
+        });
+    });
+    describe("Royalties", function () {
+        it("Should return the correct royalties", async function () {
+            await tacvue721a.connect(owner).saleActiveSwitch();
+            await tacvue721a.connect(addr1).mint(1, {
+                value: ethers.utils.parseEther("1")
+            });
+            let royalty = [
+                '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+               { value: '20000000000000000' }
+              ]
+            let check = await tacvue721a.royaltyInfo(1, ethers.utils.parseEther("1"))
 
-}
-);
+            expect(check[0]).to.equal(royalty[0]);
+            expect(check[1]).to.equal(royalty[1].value);
+        });
+        it("Should fail if the royalties are not correct", async function () {
+            await tacvue721a.connect(owner).saleActiveSwitch();
+            await tacvue721a.connect(addr1).mint(1, {
+                value: ethers.utils.parseEther("1")
+            });
+            let royalty = [
+                '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+               { value: '40000000000000000' }
+              ]
+            let check = await tacvue721a.royaltyInfo(1, ethers.utils.parseEther("1"))
+
+            expect(check[0]).to.equal(royalty[0]);
+            expect(check[1]).to.not.equal(royalty[1].value);
+        }
+        );
+
+    });
+
+});
