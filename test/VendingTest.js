13162d940dcd6f833ef1ec7b5f411cef0da718f2
BradMyrick <brad.myrick@protonmail.com>
2022-11-21 08:33:00 -0500

    cleaned to push over to trugard

--- test/VendingTest.js
+++ test/VendingTest.js
@@ -9,9 +9,9 @@ const {
     parseEther
 } = require("ethers/lib/utils");
 
-describe("RxgVending contract", function () {
-    let RxgVending;
-    let rxgVending;
+describe("ERC20Vending contract", function () {
+    let ERC20Vending;
+    let erc20Vending;
     let peggedPrice = 1000;
     let RxgToken;
     let rxgToken;
@@ -21,85 +21,85 @@ describe("RxgVending contract", function () {
         [owner, addr1, addr2, addr3, buyer, ...addrs] = await ethers.getSigners();
         RxgToken = await ethers.getContractFactory("Recharge");
         rxgToken = await RxgToken.connect(owner).deploy(addr1.address, addr2.address, addr3.address);
-        RxgVending = await ethers.getContractFactory("RxgVending");
-        rxgVending = await RxgVending.connect(owner).deploy(peggedPrice, rxgToken.address);
-        await rxgVending.deployed();
+        ERC20Vending = await ethers.getContractFactory("ERC20Vending");
+        erc20Vending = await ERC20Vending.connect(owner).deploy(peggedPrice, rxgToken.address);
+        await erc20Vending.deployed();
     });
     describe("Deployment", function () {
         it("Should set the right owner", async function () {
-            expect(await rxgVending.owner()).to.equal(owner.address);
+            expect(await erc20Vending.owner()).to.equal(owner.address);
         });
     });
     describe("Change Price", function () {
         it("Should change the price if owner", async function () {
-            await rxgVending.connect(owner).changePrice(parseEther("1"));
-            expect(await rxgVending.rxgPerAvax()).to.equal(parseEther("1"));
+            await erc20Vending.connect(owner).changePrice(parseEther("1"));
+            expect(await erc20Vending.tokenPerAvax()).to.equal(parseEther("1"));
         });
         it("Should not change the price if not owner", async function () {
-            await (expect(rxgVending.connect(addr1).changePrice(parseEther("1"))).to.be.reverted);
+            await (expect(erc20Vending.connect(addr1).changePrice(parseEther("1"))).to.be.reverted);
         });
     });
     describe("Add Supply", function () {
         it("Should add supply if approved", async function () {
-            await rxgToken.connect(addr1).approve(rxgVending.address, parseEther("1"));
-            await rxgVending.connect(addr1).addRxg(parseEther("1"));
-            expect(await rxgToken.balanceOf(rxgVending.address)).to.equal(parseEther("1"));
+            await rxgToken.connect(addr1).approve(erc20Vending.address, parseEther("1"));
+            await erc20Vending.connect(addr1).addERC20(parseEther("1"));
+            expect(await rxgToken.balanceOf(erc20Vending.address)).to.equal(parseEther("1"));
         });
         it("Should not add supply if not approved", async function () {
-            await (expect(rxgVending.connect(addr1).addRxg(parseEther("1"))).to.be.reverted);
+            await (expect(erc20Vending.connect(addr1).addERC20(parseEther("1"))).to.be.reverted);
         });
         it("Should revert if you don't have enough approval", async function () {
-            await rxgToken.connect(addr1).approve(rxgVending.address, parseEther("1"));
-            await (expect(rxgVending.connect(addr1).addRxg(parseEther("2"))).to.be.reverted);
+            await rxgToken.connect(addr1).approve(erc20Vending.address, parseEther("1"));
+            await (expect(erc20Vending.connect(addr1).addERC20(parseEther("2"))).to.be.reverted);
         });
-        it("Should add the rxg to the supply no matter what account you use", async function () {
-            await rxgToken.connect(addr1).approve(rxgVending.address, parseEther("1"));
-            await rxgVending.connect(addr1).addRxg(parseEther("1"));
-            await rxgToken.connect(addr2).approve(rxgVending.address, parseEther("2"));
-            await rxgVending.connect(addr2).addRxg(parseEther("2"));
-            expect(await rxgToken.balanceOf(rxgVending.address)).to.equal(parseEther("3"));
+        it("Should add the erc20 to the supply no matter what account you use", async function () {
+            await rxgToken.connect(addr1).approve(erc20Vending.address, parseEther("1"));
+            await erc20Vending.connect(addr1).addERC20(parseEther("1"));
+            await rxgToken.connect(addr2).approve(erc20Vending.address, parseEther("2"));
+            await erc20Vending.connect(addr2).addERC20(parseEther("2"));
+            expect(await rxgToken.balanceOf(erc20Vending.address)).to.equal(parseEther("3"));
         });
     });
-    describe("Buy Rxg", function () {
-        it("Should buy rxg if you have enough rxg", async function () {
-            await rxgToken.connect(addr1).approve(rxgVending.address, parseEther("1000"));
-            await rxgVending.connect(addr1).addRxg(parseEther("1000"));
-            expect(await rxgToken.balanceOf(rxgVending.address)).to.equal(parseEther("1000"));
-            await rxgVending.connect(buyer).buyRxg({
+    describe("Buy Erc20", function () {
+        it("Should buy erc20 if you have enough Base Token", async function () {
+            await rxgToken.connect(addr1).approve(erc20Vending.address, parseEther("1000"));
+            await erc20Vending.connect(addr1).addERC20(parseEther("1000"));
+            expect(await rxgToken.balanceOf(erc20Vending.address)).to.equal(parseEther("1000"));
+            await erc20Vending.connect(buyer).buyERC20({
                 value: parseEther("1")
             });
-            expect(await rxgToken.balanceOf(rxgVending.address)).to.equal(0);
+            expect(await rxgToken.balanceOf(erc20Vending.address)).to.equal(0);
         });
-        it("Should not buy rxg if you don't have enough Avax", async function () {
+        it("Should not buy erc20 if you don't have enough Avax", async function () {
 
-            await (expect(rxgVending.connect(addr1).buyRxg({
+            await (expect(erc20Vending.connect(addr1).buyERC20({
                 value: 0
             })).to.be.reverted);
         });
-        it("Should not buy rxg if the Vending contract doesn't have enough rxg", async function () {
-            await rxgToken.connect(addr1).approve(rxgVending.address, parseEther("1"));
-            await (expect(rxgVending.connect(addr1).buyRxg({
+        it("Should not buy erc20 if the Vending contract doesn't have enough erc20", async function () {
+            await rxgToken.connect(addr1).approve(erc20Vending.address, parseEther("1"));
+            await (expect(erc20Vending.connect(addr1).buyERC20({
                 value: parseEther("1")
             })).to.be.reverted);
         });
     });
     describe("Withdraw", function () {
         it("Should withdraw if owner", async function () {
-            await rxgToken.connect(addr1).approve(rxgVending.address, parseEther("1000"));
-            await rxgVending.connect(addr1).addRxg(parseEther("1000"));
-            expect(await rxgToken.balanceOf(rxgVending.address)).to.equal(parseEther("1000"));
-            await rxgVending.connect(buyer).buyRxg({
+            await rxgToken.connect(addr1).approve(erc20Vending.address, parseEther("1000"));
+            await erc20Vending.connect(addr1).addERC20(parseEther("1000"));
+            expect(await rxgToken.balanceOf(erc20Vending.address)).to.equal(parseEther("1000"));
+            await erc20Vending.connect(buyer).buyERC20({
                 value: parseEther("1")
             });
-            // get ether balance of rxgVending contract
-            tx = await rxgVending.connect(owner).withdraw();
+            // get ether balance of erc20Vending contract
+            tx = await erc20Vending.connect(owner).withdraw();
             results = await tx.wait();
             for (const event of results.events) {
                 console.log(`Event ${event.event} with args ${event.args}`);
             }
         });
         it("Should not withdraw if not owner", async function () {
-            await (expect(rxgVending.connect(addr1).withdraw()).to.be.reverted);
+            await (expect(erc20Vending.connect(addr1).withdraw()).to.be.reverted);
         });
     });
 
